3807855c3f77f684ba1788bc6d691441c168c02f
Xzaohui <51278354+Xzaohui@users.noreply.github.com>
2022-05-04 03:06:55 +0800

    Delete pre_train.py

--- pre_train.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-from gensim.models import Word2Vec
-from gensim.models.word2vec import LineSentence
-# model = Word2Vec(LineSentence('enwik9'), size = 200, window = 5, min_count = 5, workers = multiprocessing.cpu_count())
-# model.save('chemical.model')
-
-model=Word2Vec.load('chemical.model')
-print(model['chemical'])
