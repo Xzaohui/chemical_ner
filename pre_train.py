19e94fa1427561879d10e1f09b46f5143df1dbc2
Xzaohui <51278354+Xzaohui@users.noreply.github.com>
2022-06-20 11:14:10 +0800

    Delete pre_train.py

--- pre_train.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import word2vec
-sentences = word2vec.Text8Corpus('./model/enwik9')
-model = word2vec.Word2Vec(sentences, size=100)
-word2vec.save('./model/chemical.model')
-print(model.most_similar('oxygen'))
