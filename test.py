4758379f5880067881be7375ed67d3752da618d8
Xzaohui <zaohuixu@gmail.com>
2022-10-26 09:55:13 +0000

    1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 import datetime
-from gensim.models import word2vec
 import torch
 import torch.nn as nn
 
-torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+a=torch.tensor(1,2)
+print(a)
