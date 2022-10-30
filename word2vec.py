05256ef99f0bec145864f9a827446dbbde81d0e8
Xzaohui <839126714@qq.com>
2022-10-30 15:16:14 +0800

    nothing

--- word2vec.py
+++ word2vec.py
@@ -1,6 +1,3 @@
-from gensim.models import word2vec
-
-sentences = word2vec.Text8Corpus('./data/word.txt')
-model = word2vec.Word2Vec(sentences, size=256)
-model.save('./model/chemical256.w2v')
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:c90771bff240e3ed0d4cee7d67cebc111b13c791fd93cc43a0c69a48d18a015d
+size 180
